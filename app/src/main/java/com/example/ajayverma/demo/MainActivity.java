fe1c3b30f21533e19c0945dc77ec6a06d5676ddf
Ajay verma <vajay1497@gmail.com>
2017-04-25 21:09:23 +0530

    list view

--- app/src/main/java/com/example/ajayverma/demo/MainActivity.java
+++ app/src/main/java/com/example/ajayverma/demo/MainActivity.java
@@ -8,44 +8,44 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    ListView list_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        list_name = (ListView) findViewById(R.id.listView);
+        String[] values = new String[]{"Sachin Tendulkar",
+                "ViratKohli",
+                "Rohit Sharma",
+                "Suresh Raina",
+                "Gautam Gambhir",
+                "Rahul Dravid"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.cricktername, R.id.textView, values);
+        list_name.setAdapter(adapter);
+        AdapterView list_view;
+        list_name.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int itemPosition = position;
+                String itemValue = (String) list_name.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(),
+                        "Position:" + itemPosition + "ListItem:" + itemValue, Toast.LENGTH_LONG)
+                        .show();
+
+
             }
         });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-
-        int id = item.getItemId();
-
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
+
+
