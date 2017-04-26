049824797007fe9ba5ce5f10b3dd7e07c83fb265
Ajay verma <vajay1497@gmail.com>
2017-04-26 20:39:22 +0530

    changes in both the files

--- app/src/main/java/com/example/ajayverma/demo/MainActivity.java
+++ app/src/main/java/com/example/ajayverma/demo/MainActivity.java
@@ -10,42 +10,41 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.app.Activity;
+import android.content.Intent;
 import android.widget.Toast;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
-    ListView list_name;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageView i;
+    Button b_ok;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        i = (ImageView) findViewById(R.id.ImageView);
+        b_ok = (Button) findViewById(R.id.Button);
 
 
-        list_name = (ListView) findViewById(R.id.listView);
-        String[] values = new String[]{"Sachin Tendulkar",
-                "ViratKohli",
-                "Rohit Sharma",
-                "Suresh Raina",
-                "Gautam Gambhir",
-                "Rahul Dravid"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.cricktername, R.id.textView, values);
-        list_name.setAdapter(adapter);
-        AdapterView list_view;
-        list_name.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                int itemPosition = position;
-                String itemValue = (String) list_name.getItemAtPosition(position);
-                Toast.makeText(getApplicationContext(),
-                        "Position:" + itemPosition + "ListItem:" + itemValue, Toast.LENGTH_LONG)
-                        .show();
+        i.setOnClickListener(this);
+        b_ok.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
 
-            }
-        });
+        if (view.getId() == R.id.ImageView) {
+            i = setVisible(View.GONE);
 
+            b_ok = setVisible(View.INVISIBLE);
+        } else {
+            b_ok = setVisible(View.GONE);
+            i = setVisible(View.INVISIBLE);
+        }
     }
 }
-
-
