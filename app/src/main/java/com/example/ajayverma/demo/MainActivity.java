4868d5cb96f12a396a79f1a05fb4066724ddf699
Ajay verma <vajay1497@gmail.com>
2017-04-28 23:53:12 +0530

    list of item created in which new item can created

--- app/src/main/java/com/example/ajayverma/demo/MainActivity.java
+++ app/src/main/java/com/example/ajayverma/demo/MainActivity.java
@@ -1,50 +1,56 @@
 package com.example.ajayverma.demo;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
+import android.widget.EditText;
 import android.widget.ListView;
-import android.app.Activity;
-import android.content.Intent;
 import android.widget.Toast;
 import android.widget.Button;
-import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    ImageView i;
-    Button b_ok;
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    private EditText itemName;
+    private ListView itemListView;
+    private Button clickButton;
+    private ArrayAdapter <String> itemListAdapter;
+    private ArrayList<String> itemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        i = (ImageView) findViewById(R.id.ImageView);
-        b_ok = (Button) findViewById(R.id.Button);
+        setContentView(R.layout.dataentry);
+        itemName = (EditText) findViewById(R.id.EnterItem_name);
+        clickButton = (Button) findViewById(R.id.Click);
+        itemListView = (ListView) findViewById(R.id.list_item);
+
+        itemList = new ArrayList<String>();
+
+        itemListAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemList);
+        itemListView.setAdapter(itemListAdapter);
+
+        clickButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                itemList.add(itemName.getText().toString());
+                itemName.setText("");
+                itemListAdapter.notifyDataSetChanged();
+            }
+        });
+
+        itemListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> a, View v, int position,
+                                    long id) {
+                Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
 
 
-        i.setOnClickListener(this);
-        b_ok.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
-
-        if (view.getId() == R.id.ImageView) {
-            i = setVisible(View.GONE);
-
-            b_ok = setVisible(View.INVISIBLE);
-        } else {
-            b_ok = setVisible(View.GONE);
-            i = setVisible(View.INVISIBLE);
-        }
-    }
 }
